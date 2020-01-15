a585693287e02dbe7ea996abec88e0fdbdf5295d
mplyusnin <mplyusnin@skbkontur.ru>
2020-01-15 16:28:00 +0500

    add simple application

---
+++ SampleApp/Middleware/TokenMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace SampleApp.Middleware
+{
+    public class TokenMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public TokenMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+
+            if (token.Equals("12345"))
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid.").ConfigureAwait(false);
+            }
+            else
+            {
+                await next.Invoke(context).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ SampleApp/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SampleApp.Middleware;
+
+namespace SampleApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/",
+            //        context => context.Response.WriteAsync($"Application name: {env.ApplicationName}"));
+            //});
+
+            //app.Map("/home", home =>
+            //{
+            //    home.Map("/index", Index);
+            //    home.Map("/about", About);
+            //});
+
+            app.UseMiddleware<TokenMiddleware>();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Page Not Found");
+            });
+        }
+
+        private static void Index(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Index");
+            });
+        }
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("About");
+            });
+        }
+    }
+}
