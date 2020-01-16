00a05923a57f1be9bef3c21f26e80ca3e8385d5a
mplyusnin <mplyusnin@skbkontur.ru>
2020-01-16 18:17:44 +0500

    Основы конфигурации

--- SampleApp/Startup.cs
+++ SampleApp/Startup.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.StaticFiles.Infrastructure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SampleApp.Middleware;
@@ -14,44 +17,28 @@ namespace SampleApp
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            AppConfiguration = configuration;
+        }
+
+        public IConfiguration AppConfiguration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddTransient<ICounter, RandomCounter>();
-            //services.AddTransient<CounterService>();
-
-            //services.AddScoped<ICounter, RandomCounter>();
-            //services.AddScoped<CounterService>();
-
-            services.AddSingleton<ICounter, RandomCounter>();
-            services.AddSingleton<CounterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(
-            IApplicationBuilder app,
-            IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //env.EnvironmentName = "Production";
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMiddleware<CounterMiddleware>();
-
-            app.Run(async (context) =>
-            {
-                //не рекомендуется так делать, но можно
-                var messageSender = context.RequestServices.GetService<IMessageSender>();
-
-                messageSender = app.ApplicationServices.GetService<IMessageSender>();
-
-                await context.Response.WriteAsync(messageSender.Send())
-                    .ConfigureAwait(false);
-            });
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello").ConfigureAwait(false); });
         }
     }
 }
