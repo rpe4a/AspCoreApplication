9e46788d2d46a7eb8854fd7853788e6b79958c43
mplyusnin <mplyusnin@skbkontur.ru>
2020-01-20 14:51:33 +0500

    Добавление MVC в пустой проект

--- SampleApp/Startup.cs
+++ SampleApp/Startup.cs
@@ -46,9 +46,10 @@ namespace SampleApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddSingleton<IConfiguration>((p) => AppConfiguration);
             services.AddSingleton<Config>((p) => AppConfiguration.Get<Config>());
-            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,43 +60,14 @@ namespace SampleApp
                 app.UseDeveloperExceptionPage();
             }
 
-            var myRouteHandler = new RouteHandler(Handle);
-            var routeBuilder = new RouteBuilder(app, myRouteHandler);
-            routeBuilder.MapRoute("default", "{action=Index}/{name}-{year}");
-            routeBuilder.MapRoute(
-                name: "default",
-                template: "{controller}/{action}/{id?}",
-                defaults: new { controller = "Home", action = "Index" },
-                constraints: new
-                {
-                    action = new CompositeRouteConstraint(new IRouteConstraint[]
-                    {
-                        new AlphaRouteConstraint(),
-                        new MinLengthRouteConstraint(5)
-                    })
-                });
-            routeBuilder.MapRoute("default", "{controller:regex(^H.*)}/{action}/{id?}");
-            routeBuilder.MapRoute("default", "{controller:length(4)}/{action:alpha}/{id:range(4,100)}");
-            routeBuilder.MapRoute(
-                name: "default",
-                template: "{controller}/{action:alpha:minlength(5)}/{id?}",
-                defaults: new { controller = "Home", action = "Index" });
+            app.UseRouting(); // используем систему маршрутизации
 
-            app.UseRouter(routeBuilder.Build());
-
-            app.Run(async (context) =>
+            app.UseEndpoints(endpoints =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
-        }
-
-        private async Task Handle(HttpContext context)
-        {
-            var routeValues = context.GetRouteData().Values;
-            var action = routeValues["action"].ToString();
-            var name = routeValues["name"].ToString();
-            var year = routeValues["year"].ToString();
-            await context.Response.WriteAsync($"action: {action} | name: {name} | year:{year}");
         }
     }
 }
