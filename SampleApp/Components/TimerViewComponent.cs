a3736fdf57749fac91a916cd51edf8eb014af584
mplyusnin <mplyusnin@skbkontur.ru>
2020-01-27 15:43:23 +0500

    Определение компонента представлений

---
+++ SampleApp/Components/TimerViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleApp.Components
+{
+    public class Timer : ViewComponent
+    {
+        public Task<string> InvokeAsync()
+        {
+            return Task.FromResult($"Текущее время: {DateTime.Now.ToString("hh:mm:ss")}");
+        }
+    }
+}
