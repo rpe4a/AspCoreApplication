f707b051279b9f4b2d1cfe8ff7ff000c2aae10a3
mplyusnin <mplyusnin@skbkontur.ru>
2020-01-27 15:32:51 +0500

    Tag-хелперы и модели

---
+++ SampleApp/App_Code/ListTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SampleApp.App_Code
+{
+    public class ListTagHelper : TagHelper
+    {
+        public List<string> Elements { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";
+            string listContent = "";
+            foreach (string item in Elements)
+                listContent += "<li>" + item + "</li>";
+
+            output.Content.SetHtmlContent(listContent);
+        }
+    }
+}
