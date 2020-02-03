b4c3fa674c837c2c14c25ac7887316142ffb2607
mplyusnin <mplyusnin@skbkontur.ru>
2020-02-03 10:32:26 +0500

    Фильтр ресурсов

---
+++ SampleApp/Filters/IEAsyncFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SampleApp.Filters
+{
+    public class IEAsyncFilterAttribute : Attribute, IAsyncResourceFilter
+    {
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            // получаем информацию о браузере пользователя
+            string userAgent = context.HttpContext.Request.Headers["User-Agent"].ToString();
+            if (Regex.IsMatch(userAgent, "MSIE|Trident"))
+            {
+                context.Result = new ContentResult { Content = "Ваш браузер устарел" };
+            }
+            else    // если браузер не IE, передаем обаботку запроса дальше
+                await next();
+        }
+    }
+}
---
+++ SampleApp/Filters/IEFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SampleApp.Filters
+{
+    public class IEFilterAttribute : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            // получаем информацию о браузере пользователя
+            string userAgent = context.HttpContext.Request.Headers["User-Agent"].ToString();
+            if (Regex.IsMatch(userAgent, "MSIE|Trident"))
+            {
+                context.Result = new ContentResult { Content = "Ваш браузер устарел" };
+            }
+        }
+    }
+}
