78065bb345a3fffc81187a61543870eeb4a64e9e
mplyusnin <mplyusnin@skbkontur.ru>
2020-02-03 11:29:18 +0500

    Фильтры исключений

---
+++ SampleApp/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SampleApp.Filters
+{
+    public class CustomExceptionFilterAttribute : Attribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            string actionName = context.ActionDescriptor.DisplayName;
+            string exceptionStack = context.Exception.StackTrace;
+            string exceptionMessage = context.Exception.Message;
+            context.Result = new ContentResult
+            {
+                Content = $"В методе {actionName} возникло исключение: \n {exceptionMessage} \n {exceptionStack}"
+            };
+            context.ExceptionHandled = true;
+        }
+    }
+}
