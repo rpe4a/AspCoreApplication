5b249428824026383521d9d637b1390962f9d43d
mplyusnin <mplyusnin@skbkontur.ru>
2020-02-11 10:40:28 +0500

    Локализация аннотаций данных

--- SampleApp/Models/Person.cs
+++ SampleApp/Models/Person.cs
@@ -7,10 +7,18 @@ namespace SampleApp.Models
     [NamePasswordEqual]
     public class Person : IValidatableObject
     {
+        [Required(ErrorMessage = "NameRequired")]
+        [StringLength(20, ErrorMessage = "NameLength", MinimumLength = 6)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
         public string Email { get; set; }
+
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "AgeRequired")]
+        [Range(10, 100, ErrorMessage = "AgeRange")]
+        [Display(Name = "Age")]
         public int Age { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
