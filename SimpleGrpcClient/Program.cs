6cb60c99db2296f4e2682a6c77407055a6e64baa
mplyusnin <mplyusnin@skbkontur.ru>
2020-02-14 15:49:37 +0500

    Введение в gRPC

---
+++ SimpleGrpcClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using SimpleGrpcService;
+
+namespace SimpleGrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            Console.WriteLine("Введите ваше имя:");
+
+            string name = Console.ReadLine();
+
+            var reply = await client.SayHelloAsync(new HelloRequest() {Name = name});
+            Console.WriteLine("Ответ сервера: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
